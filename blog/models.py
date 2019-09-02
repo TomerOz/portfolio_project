169e087f3a0f0fca3d0438fa578603b3e1ab6f9f
Tomer Oz <tomer.oz22@gmail.com>
2019-09-02 20:46:33 +0300

    saving evening

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
 class Blog(models.Model):
-	text = models.TextField(max_length=400)
+	# text
+	title = models.CharField(max_length=255)
+	# publication date
+	pubish_date = models.DateTimeField()
+	# body
+	text = models.TextField(max_length=800)
+	# image
+	image = models.ImageField(upload_to='images/')
+
+	
+	
+# a model creation flow:
+	# create a model class
+	# add model app to the setting
+	# make new migrations
+	# migrate
+	# add to admin
