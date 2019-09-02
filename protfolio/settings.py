1539b19b821c628dfe86552e120229b747c57b65
Tomer Oz <tomer.oz22@gmail.com>
2019-09-02 10:51:30 +0300

    progress save

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'protfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'protfolionew',
+		'USER': 'postgres',
+		'PASSWORD': 'tomerstav',
+		'HOST': 'localhost',
+		'PORT' : '5432',
     }
 }
 
@@ -118,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# I've created these two contatnts and I use them in jobs.admin
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#doesnt have to be the same as media_root
+MEDIA_URL = '/media/' 
+
